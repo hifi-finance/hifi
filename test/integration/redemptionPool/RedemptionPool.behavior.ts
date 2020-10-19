ab60b2327f4205dc4c8b09b41e3f7ee2e5cb1c6a
Paul Razvan Berg <hello@paulrberg.com>
2020-10-19 15:12:53 +0300

    chore(protocol): refactor: fix stale reference to 'shouldBehaveLikeRedeemYTokens'

--- test/integration/redemptionPool/RedemptionPool.behavior.ts
+++ test/integration/redemptionPool/RedemptionPool.behavior.ts
@@ -1,10 +1,10 @@
-import shouldBehaveLikeRedeemUnderlying from "./effects/redeemUnderlying";
+import shouldBehaveLikeRedeemYTokens from "./effects/redeemYTokens";
 import shouldBehaveLikeSupplyUnderlying from "./effects/supplyUnderlying";
 
 export function shouldBehaveLikeRedemptionPool(): void {
   describe("Effects Functions", function () {
-    describe("redeemUnderlying", function () {
-      shouldBehaveLikeRedeemUnderlying();
+    describe("redeemYTokens", function () {
+      shouldBehaveLikeRedeemYTokens();
     });
 
     describe("supplyUnderlying", function () {
