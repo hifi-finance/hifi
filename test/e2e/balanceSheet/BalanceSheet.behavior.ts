63573f65e0d4eb2c50a84d415ea8a6994161dd53
Paul Razvan Berg <hello@paulrberg.com>
2020-10-24 23:09:24 +0300

    chore(protocol): test: temporarily remove e2e tests

--- test/e2e/balanceSheet/BalanceSheet.behavior.ts
+++
@@ -1,9 +0,0 @@
-import shouldBehaveLikeDepositCollateral from "./effects/depositCollateral";
-
-export function shouldBehaveLikeBalanceSheet(): void {
-  describe("Effects Functions", function () {
-    describe("depositCollateral", function () {
-      shouldBehaveLikeDepositCollateral();
-    });
-  });
-}
