35d6ec576bfb20c5a5ca9d33866e35f66356bcf0
Paul Razvan Berg <hello@paulrberg.com>
2020-11-25 20:37:42 +0200

    chore(protocol): feat: update deployment script for proxy target

--- scripts/3_deploy_batterseaScriptsV1.ts
+++ scripts/3_deploy_batterseaScriptsV1.ts
@@ -1,11 +1,26 @@
 import { Contract, ContractFactory } from "@ethersproject/contracts";
 import { ethers } from "hardhat";
 
-const wethAddress: string = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+let balancerExchangeProxyAddress: string;
+if (!process.env.BALANCER_EXCHANGE_PROXY_ADDRESS) {
+  throw new Error("Please set BALANCER_EXCHANGE_PROXY_ADDRESS as an env variable");
+} else {
+  balancerExchangeProxyAddress = process.env.BALANCER_EXCHANGE_PROXY_ADDRESS;
+}
+
+let wethAddress: string;
+if (!process.env.WETH_ADDRESS) {
+  throw new Error("Please set WETH_ADDRESS as an env variable");
+} else {
+  wethAddress = process.env.WETH_ADDRESS;
+}
 
 async function main(): Promise<void> {
   const batterseaScriptsV1Factory: ContractFactory = await ethers.getContractFactory("BatterseaScriptsV1");
-  const batterseaScriptsV1: Contract = await batterseaScriptsV1Factory.deploy(wethAddress);
+  const batterseaScriptsV1: Contract = await batterseaScriptsV1Factory.deploy(
+    balancerExchangeProxyAddress,
+    wethAddress,
+  );
   await batterseaScriptsV1.deployed();
 
   console.log("BatterseaScriptsV1 deployed to: ", batterseaScriptsV1.address);
