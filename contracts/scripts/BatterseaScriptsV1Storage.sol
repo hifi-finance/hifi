2fde7c129805e7b7cb59d18dcb487d0b15b194dd
Paul Razvan Berg <hello@paulrberg.com>
2020-12-24 14:45:47 +0200

    chore(protocol): feat: change mainnet addresses with the kovan versions

--- contracts/scripts/BatterseaScriptsV1Storage.sol
+++ contracts/scripts/BatterseaScriptsV1Storage.sol
@@ -11,11 +11,11 @@ abstract contract BatterseaScriptsV1Storage {
      * @notice The contract that enables trading on the Balancer Exchange.
      * @dev This is the mainnet version of the Exchange Proxy. Change it with the testnet version when needed.
      */
-    address public constant EXCHANGE_PROXY_ADDRESS = 0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21;
+    address public constant EXCHANGE_PROXY_ADDRESS = 0x4e67bf5bD28Dd4b570FBAFe11D0633eCbA2754Ec;
 
     /**
      * @notice The contract that enables wrapping ETH into ERC-20 form.
      * @dev This is the mainnet version of WETH. Change it with the testnet version when needed.
      */
-    address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant WETH_ADDRESS = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
 }
