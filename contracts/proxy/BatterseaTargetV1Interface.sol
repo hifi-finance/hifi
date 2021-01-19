c154b0bf1484243026298e9f1d10d7020785a261
Paul Razvan Berg <hello@paulrberg.com>
2021-01-19 12:14:49 +0200

    chore(proxy-target): refactor: rename BatterseaScriptsV1 to BatterseaTargetV1
    refactor: rename "scripts" folder to "proxy"

---
+++ contracts/proxy/BatterseaTargetV1Interface.sol
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+pragma solidity ^0.7.0;
+
+import "./BatterseaTargetV1Storage.sol";
+
+/**
+ * @title BatterseaTargetV1Interface
+ * @author Hifi
+ * @notice Storage contract for BatterseaTargetV1.
+ */
+abstract contract BatterseaTargetV1Interface is BatterseaTargetV1Storage {
+    event BorrowAndSellFyTokens(
+        address indexed borrower,
+        uint256 borrowAmount,
+        uint256 fyTokenDelta,
+        uint256 underlyingAmount
+    );
+}
---
+++ contracts/proxy/BatterseaTargetV1Storage.sol
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+pragma solidity ^0.7.0;
+
+/**
+ * @title BatterseaTargetV1Storage
+ * @author Hifi
+ * @notice Storage contract for BatterseaTargetV1.
+ */
+abstract contract BatterseaTargetV1Storage {
+    /**
+     * @notice The contract that enables trading on the Balancer Exchange.
+     * @dev This is the mainnet version of the Exchange Proxy. Change it with the testnet version when needed.
+     */
+    address public constant EXCHANGE_PROXY_ADDRESS = 0x3E66B66Fd1d0b02fDa6C811Da9E0547970DB2f21;
+
+    /**
+     * @notice The contract that enables wrapping ETH into ERC-20 form.
+     * @dev This is the mainnet version of WETH. Change it with the testnet version when needed.
+     */
+    address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+}
