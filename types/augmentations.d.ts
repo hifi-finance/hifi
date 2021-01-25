fa1a17934728e2ff2a7357695d94032793762307
Paul Razvan Berg <hello@paulrberg.com>
2021-01-25 11:10:20 +0200

    chore(flash-swap): chore: remove stale "Accounts" type

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -1,7 +1,6 @@
-import { Accounts, Contracts, Signers } from "./";
+import { Contracts, Signers } from "./";
 declare module "mocha" {
   export interface Context {
-    accounts: Accounts;
     contracts: Contracts;
     signers: Signers;
   }
