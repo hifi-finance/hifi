6ee327b48ba7f1ebf2a6a0e75ac5ca32920551d9
Paul Razvan Berg <hello@paulrberg.com>
2021-02-15 20:00:44 +0200

    chore(flash-swap): refactor: use the hardhat way to import contract artifact

--- scripts/create_liquidation_opportunity.ts
+++ scripts/create_liquidation_opportunity.ts
@@ -1,18 +1,16 @@
-import BalanceSheetArtifact from "@hifi/protocol/artifacts/BalanceSheet.json";
-import FyTokenArtifact from "@hifi/protocol/artifacts/FyToken.json";
-import UniswapV2PairArtifact from "@uniswap/v2-core/build/UniswapV2Pair.json";
+import balanceSheetArtifact from "@hifi/protocol/artifacts/BalanceSheet.json";
+import fyTokenArtifact from "@hifi/protocol/artifacts/FyToken.json";
+import hre from "hardhat";
+import uniswapV2PairArtifact from "@uniswap/v2-core/build/UniswapV2Pair.json";
 
+import { Artifact } from "hardhat/types";
 import { BalanceSheet } from "@hifi/protocol/typechain/BalanceSheet";
 import { BigNumber } from "@ethersproject/bignumber";
 import { FyToken } from "@hifi/protocol/typechain/FyToken";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { ethers } from "hardhat";
 
-import DummyPriceFeedArtifact from "../artifacts/contracts/test/DummyPriceFeed.sol/DummyPriceFeed.json";
-import GodModeErc20Artifact from "../artifacts/contracts/test/GodModeErc20.sol/GodModeErc20.json";
-
-import { DummyPriceFeed } from "../typechain/DummyPriceFeed";
 import { GodModeErc20 } from "../typechain/GodModeErc20";
+import { SimplePriceFeed } from "../typechain/SimplePriceFeed";
 import { UniswapV2Pair } from "../types/contracts/UniswapV2Pair";
 import {
   getPartialWbtcAmount,
@@ -28,6 +26,8 @@ const p20k: BigNumber = getWholeOraclePrice(20000);
 const usdc1dot25m = getWholeUsdcAmount(1250000);
 const wbtcdot88: BigNumber = getPartialWbtcAmount(100).mul(88);
 const wbtc100 = getWholeWbtcAmount(100);
+
+const { ethers } = hre;
 
 function getEnvVar(key: string): string {
   if (!process.env[key]) {
@@ -46,20 +46,24 @@ async function main(): Promise<void> {
   const signers: SignerWithAddress[] = await ethers.getSigners();
 
   const balanceSheet: BalanceSheet = <BalanceSheet>(
-    new ethers.Contract(getEnvVar("BALANCE_SHEET_ADDRESS"), BalanceSheetArtifact.abi, signers[0])
+    new ethers.Contract(getEnvVar("BALANCE_SHEET_ADDRESS"), balanceSheetArtifact.abi, signers[0])
   );
-  const fyToken: FyToken = <FyToken>new ethers.Contract(getEnvVar("FY_TOKEN_ADDRESS"), FyTokenArtifact.abi, signers[0]);
+  const fyToken: FyToken = <FyToken>new ethers.Contract(getEnvVar("FY_TOKEN_ADDRESS"), fyTokenArtifact.abi, signers[0]);
   const uniV2WbtcUsdc: UniswapV2Pair = <UniswapV2Pair>(
-    new ethers.Contract(getEnvVar("UNI_V2_WBTC_USDC_ADDRESS"), UniswapV2PairArtifact.abi, signers[0])
+    new ethers.Contract(getEnvVar("UNI_V2_WBTC_USDC_ADDRESS"), uniswapV2PairArtifact.abi, signers[0])
   );
+
+  const godModeErc20Artifact: Artifact = await hre.artifacts.readArtifact("GodModeErc20");
   const usdc: GodModeErc20 = <GodModeErc20>(
-    new ethers.Contract(getEnvVar("USDC_ADDRESS"), GodModeErc20Artifact.abi, signers[0])
+    new ethers.Contract(getEnvVar("USDC_ADDRESS"), godModeErc20Artifact.abi, signers[0])
   );
   const wbtc: GodModeErc20 = <GodModeErc20>(
-    new ethers.Contract(getEnvVar("WBTC_ADDRESS"), GodModeErc20Artifact.abi, signers[0])
+    new ethers.Contract(getEnvVar("WBTC_ADDRESS"), godModeErc20Artifact.abi, signers[0])
   );
-  const wbtcPriceFeed: DummyPriceFeed = <DummyPriceFeed>(
-    new ethers.Contract(getEnvVar("WBTC_PRICE_FEED_ADDRESS"), DummyPriceFeedArtifact.abi, signers[0])
+
+  const simplePriceFeedArtifact: Artifact = await hre.artifacts.readArtifact("SimplePriceFeed");
+  const wbtcPriceFeed: SimplePriceFeed = <SimplePriceFeed>(
+    new ethers.Contract(getEnvVar("WBTC_PRICE_FEED_ADDRESS"), simplePriceFeedArtifact.abi, signers[0])
   );
 
   // Mint 100 WBTC to the UniswapV2Pair contract.
--- test/integration/fixtures.ts
+++ test/integration/fixtures.ts
@@ -1,10 +1,12 @@
-import BalanceSheetArtifact from "@hifi/protocol/artifacts/BalanceSheet.json";
-import ChainlinkOperatorArtifact from "@hifi/protocol/artifacts/ChainlinkOperator.json";
-import FintrollerArtifact from "@hifi/protocol/artifacts/Fintroller.json";
-import FyTokenArtifact from "@hifi/protocol/artifacts/FyToken.json";
-import RedemptionPoolArtifact from "@hifi/protocol/artifacts/RedemptionPool.json";
-import UniswapV2PairArtifact from "@uniswap/v2-core/build/UniswapV2Pair.json";
+import balanceSheetArtifact from "@hifi/protocol/artifacts/BalanceSheet.json";
+import chainlinkOperatorArtifact from "@hifi/protocol/artifacts/ChainlinkOperator.json";
+import fintrollerArtifact from "@hifi/protocol/artifacts/Fintroller.json";
+import fyTokenArtifact from "@hifi/protocol/artifacts/FyToken.json";
+import hre from "hardhat";
+import redemptionPoolArtifact from "@hifi/protocol/artifacts/RedemptionPool.json";
+import uniswapV2PairArtifact from "@uniswap/v2-core/build/UniswapV2Pair.json";
 
+import { Artifact } from "hardhat/types";
 import { BalanceSheet } from "@hifi/protocol/typechain/BalanceSheet";
 import { BigNumber } from "@ethersproject/bignumber";
 import { ChainlinkOperator } from "@hifi/protocol/typechain/ChainlinkOperator";
@@ -12,11 +14,6 @@ import { Fintroller } from "@hifi/protocol/typechain/Fintroller";
 import { FyToken } from "@hifi/protocol/typechain/FyToken";
 import { RedemptionPool } from "@hifi/protocol/typechain/RedemptionPool";
 import { Signer } from "@ethersproject/abstract-signer";
-import { ethers, waffle } from "hardhat";
-
-import GodModeArtifact from "../../artifacts/contracts/test/GodModeErc20.sol/GodModeErc20.json";
-import HifiFlashSwapArtifact from "../../artifacts/contracts/HifiFlashSwap.sol/HifiFlashSwap.json";
-import SimplePriceFeedArtifact from "../../artifacts/contracts/test/SimplePriceFeed.sol/SimplePriceFeed.json";
 
 import { GodModeErc20 } from "../../typechain/GodModeErc20";
 import { HifiFlashSwap } from "../../typechain/HifiFlashSwap";
@@ -24,7 +21,7 @@ import { SimplePriceFeed } from "../../typechain/SimplePriceFeed";
 import { UniswapV2Pair } from "../../types/contracts/UniswapV2Pair";
 import { usdcConstants, wbtcConstants } from "../../helpers/constants";
 
-const { deployContract } = waffle;
+const { deployContract } = hre.waffle;
 
 type IntegrationFixtureReturnType = {
   balanceSheet: BalanceSheet;
@@ -42,25 +39,35 @@ type IntegrationFixtureReturnType = {
 export async function integrationFixture(signers: Signer[]): Promise<IntegrationFixtureReturnType> {
   const deployer: Signer = signers[0];
 
+  const godModeErc20Artifact: Artifact = await hre.artifacts.readArtifact("GodModeErc20");
   const wbtc: GodModeErc20 = <GodModeErc20>(
-    await deployContract(deployer, GodModeArtifact, [wbtcConstants.name, wbtcConstants.symbol, wbtcConstants.decimals])
+    await deployContract(deployer, godModeErc20Artifact, [
+      wbtcConstants.name,
+      wbtcConstants.symbol,
+      wbtcConstants.decimals,
+    ])
   );
-  const wbtcPriceFeed: SimplePriceFeed = <SimplePriceFeed>await deployContract(deployer, SimplePriceFeedArtifact, []);
+  const simplePriceFeedArtifact: Artifact = await hre.artifacts.readArtifact("SimplePriceFeed");
+  const wbtcPriceFeed: SimplePriceFeed = <SimplePriceFeed>await deployContract(deployer, simplePriceFeedArtifact, []);
 
   const usdc: GodModeErc20 = <GodModeErc20>(
-    await deployContract(deployer, GodModeArtifact, [usdcConstants.name, usdcConstants.symbol, usdcConstants.decimals])
+    await deployContract(deployer, godModeErc20Artifact, [
+      usdcConstants.name,
+      usdcConstants.symbol,
+      usdcConstants.decimals,
+    ])
   );
-  const usdcPriceFeed: SimplePriceFeed = <SimplePriceFeed>await deployContract(deployer, SimplePriceFeedArtifact, []);
+  const usdcPriceFeed: SimplePriceFeed = <SimplePriceFeed>await deployContract(deployer, simplePriceFeedArtifact, []);
 
-  const oracle: ChainlinkOperator = <ChainlinkOperator>await deployContract(deployer, ChainlinkOperatorArtifact, []);
+  const oracle: ChainlinkOperator = <ChainlinkOperator>await deployContract(deployer, chainlinkOperatorArtifact, []);
   await oracle.setFeed(wbtc.address, wbtcPriceFeed.address);
   await oracle.setFeed(usdc.address, usdcPriceFeed.address);
 
-  const fintroller: Fintroller = <Fintroller>await deployContract(deployer, FintrollerArtifact, []);
+  const fintroller: Fintroller = <Fintroller>await deployContract(deployer, fintrollerArtifact, []);
   await fintroller.connect(deployer).setOracle(oracle.address);
 
   const balanceSheet: BalanceSheet = <BalanceSheet>(
-    await deployContract(deployer, BalanceSheetArtifact, [fintroller.address])
+    await deployContract(deployer, balanceSheetArtifact, [fintroller.address])
   );
 
   // TODO: make the name and symbol match the expiration time.
@@ -68,7 +75,7 @@ export async function integrationFixture(signers: Signer[]): Promise<Integration
   const symbol: string = "hfyUSDC-JAN22";
   const expirationTime = BigNumber.from("1619816400");
   const fyToken: FyToken = <FyToken>(
-    await deployContract(deployer, FyTokenArtifact, [
+    await deployContract(deployer, fyTokenArtifact, [
       name,
       symbol,
       expirationTime,
@@ -81,14 +88,15 @@ export async function integrationFixture(signers: Signer[]): Promise<Integration
 
   const redemptionPoolAddress = await fyToken.redemptionPool();
   const redemptionPool = <RedemptionPool>(
-    new ethers.Contract(redemptionPoolAddress, RedemptionPoolArtifact.abi, ethers.provider)
+    new hre.ethers.Contract(redemptionPoolAddress, redemptionPoolArtifact.abi, hre.ethers.provider)
   );
 
-  const uniswapV2Pair = <UniswapV2Pair>await deployContract(deployer, UniswapV2PairArtifact, []);
+  const uniswapV2Pair = <UniswapV2Pair>await deployContract(deployer, uniswapV2PairArtifact, []);
   await uniswapV2Pair.initialize(wbtc.address, usdc.address);
 
+  const hifiFlashSwapArtifact: Artifact = await hre.artifacts.readArtifact("HifiFlashSwap");
   const hifiFlashSwap: HifiFlashSwap = <HifiFlashSwap>(
-    await deployContract(deployer, HifiFlashSwapArtifact, [balanceSheet.address, uniswapV2Pair.address])
+    await deployContract(deployer, hifiFlashSwapArtifact, [balanceSheet.address, uniswapV2Pair.address])
   );
 
   return {
