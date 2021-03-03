3c4560a3dec4afa45d4ffc605627cc2acacd1ee1
Paul Razvan Berg <hello@paulrberg.com>
2021-03-03 12:22:15 +0200

    chore(protocol): perf: remove superfluous Exponential.sol import in FyToken

--- contracts/FyToken.sol
+++ contracts/FyToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@paulrberg/contracts/access/Admin.sol";
-import "@paulrberg/contracts/math/Exponential.sol";
 import "@paulrberg/contracts/token/erc20/Erc20.sol";
 import "@paulrberg/contracts/token/erc20/Erc20Interface.sol";
 import "@paulrberg/contracts/token/erc20/Erc20Permit.sol";
