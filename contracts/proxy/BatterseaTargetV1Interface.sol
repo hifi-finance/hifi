fb5c6299d0a7ba2613085fbea28b19e42f4a63a2
Paul Razvan Berg <hello@paulrberg.com>
2021-03-05 23:24:25 +0200

    chore(proxy-target): refactor: use /// instead of /* */ in comments
    refactor: remove space between names (RedemptionPool vs Redemption Pool)

--- contracts/proxy/BatterseaTargetV1Interface.sol
+++ contracts/proxy/BatterseaTargetV1Interface.sol
@@ -1,13 +1,11 @@
-/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/// SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 import "./BatterseaTargetV1Storage.sol";
 
-/**
- * @title BatterseaTargetV1Interface
- * @author Hifi
- * @notice Storage contract for BatterseaTargetV1.
- */
+/// @title BatterseaTargetV1Interface
+/// @author Hifi
+/// @notice Storage contract for BatterseaTargetV1.
 abstract contract BatterseaTargetV1Interface is BatterseaTargetV1Storage {
     event BorrowAndSellFyTokens(
         address indexed borrower,
