581a537289b12509711bad21fdb31172b7841a99
Paul Razvan Berg <hello@paulrberg.com>
2021-05-28 17:47:05 +0300

    chore(proxy-target): refactor: ^0.8.0 to >=0.8.0 in solidity files

--- contracts/proxy/BatterseaTargetV1.sol
+++ contracts/proxy/BatterseaTargetV1.sol
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: LGPL-3.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
 import "@paulrberg/contracts/token/erc20/IErc20.sol";
 import "@paulrberg/contracts/token/erc20/SafeErc20.sol";
