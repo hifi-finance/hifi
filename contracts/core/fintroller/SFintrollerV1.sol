5c3ceb448a6db15b88f7e1b4f6d58138a6869a2f
Paul Razvan Berg <hello@paulrberg.com>
2021-06-17 13:59:35 +0300

    chore(protocol): fix: import missing IErc20.sol in SFintrollerV1.sol

--- contracts/core/fintroller/SFintrollerV1.sol
+++ contracts/core/fintroller/SFintrollerV1.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity >=0.8.0;
 
+import "@paulrberg/contracts/token/erc20/IErc20.sol";
+
 import "../hToken/IHToken.sol";
 
 /// @notice SFintrollerV1
