8c227cdadbf6448c4ea78ebb9ffb0acf0fce899a
Paul Razvan Berg <hello@paulrberg.com>
2021-06-23 19:03:53 +0300

    chore(amm): test: wrap up tests for buyUnderlying

--- test/integration/hifiPool/effects/buyUnderlying.ts
+++ test/integration/hifiPool/effects/buyUnderlying.ts
@@ -3,7 +3,8 @@ import { expect } from "chai";
 import fp from "evm-fp";
 import forEach from "mocha-each";
 
-import { EPSILON, H_TOKEN_MATURITY, MAX_UD60x18 } from "../../../../helpers/constants";
+import { EPSILON, H_TOKEN_MATURITY, MAX_UD60x18, PI } from "../../../../helpers/constants";
+import { mbn } from "../../../../helpers/math";
 import { USDC, bn, hUSDC } from "../../../../helpers/numbers";
 import { getLatestBlockTimestamp } from "../../../../helpers/provider";
 import { now } from "../../../../helpers/time";
@@ -37,31 +38,31 @@ async function testBuyUnderlying(
 }
 
 export default function shouldBehaveLikeBuyUnderlying(): void {
-  context("when the bond matured", function () {
-    beforeEach(async function () {
-      const oneHourAgo: BigNumber = now().sub(3600);
-      await this.contracts.hifiPool.connect(this.signers.alice).__godMode_setMaturity(oneHourAgo);
-    });
-
+  context("when the amount of underlying to buy is zero", function () {
     it("reverts", async function () {
-      const underlyingOut: BigNumber = USDC("10");
+      const underlyingOut: BigNumber = bn("0");
       await expect(
         this.contracts.hifiPool.connect(this.signers.alice).buyUnderlying(this.signers.alice.address, underlyingOut),
-      ).to.be.revertedWith(Errors.BondMatured);
+      ).to.be.revertedWith(Errors.BuyUnderlyingZero);
     });
   });
 
-  context("when the bond did not mature", function () {
-    context("when the amount of underlying to buy is zero", function () {
+  context("when the amount of underlying to buy is not zero", function () {
+    context("when the bond matured", function () {
+      beforeEach(async function () {
+        const oneHourAgo: BigNumber = now().sub(3600);
+        await this.contracts.hifiPool.connect(this.signers.alice).__godMode_setMaturity(oneHourAgo);
+      });
+
       it("reverts", async function () {
-        const underlyingOut: BigNumber = bn("0");
+        const underlyingOut: BigNumber = USDC("10");
         await expect(
           this.contracts.hifiPool.connect(this.signers.alice).buyUnderlying(this.signers.alice.address, underlyingOut),
-        ).to.be.revertedWith(Errors.BuyUnderlyingZero);
+        ).to.be.revertedWith(Errors.BondMatured);
       });
     });
 
-    context("when the amount of underlying to buy is not zero", function () {
+    context("when the bond did not mature", function () {
       context("when there is no underlying in the pool", function () {
         it("reverts", async function () {
           const underlyingOut: BigNumber = USDC("10");
@@ -99,6 +100,63 @@ export default function shouldBehaveLikeBuyUnderlying(): void {
           forEach(testSets).it("buys %e underlying with hTokens", async function (underlyingOut: string) {
             const virtualHTokenReserves: string = initialUnderlyingReserves;
             await testBuyUnderlying.call(this, initialUnderlyingReserves, virtualHTokenReserves, underlyingOut);
+          });
+        });
+
+        context("when liquidity was provided twice or more times", function () {
+          const extraUnderlyingReserves: string = "50";
+          const totalUnderlyingReserves: string = String(
+            mbn(initialUnderlyingReserves).add(mbn(extraUnderlyingReserves)),
+          );
+
+          beforeEach(async function () {
+            // Add extra liquidity to the pool.
+            await this.contracts.underlying
+              .connect(this.signers.alice)
+              .approve(this.contracts.hifiPool.address, USDC(extraUnderlyingReserves));
+            await this.contracts.hifiPool.connect(this.signers.alice).mint(USDC(extraUnderlyingReserves));
+          });
+
+          context("when it is the first trade", function () {
+            const testSets = [["3.141592"], ["10"], ["100"]];
+
+            forEach(testSets).it("buys %e underlying with hTokens", async function (underlyingOut: string) {
+              const virtualHTokenReserves: string = totalUnderlyingReserves;
+              await testBuyUnderlying.call(this, totalUnderlyingReserves, virtualHTokenReserves, underlyingOut);
+            });
+          });
+
+          context("when it is the second trade", function () {
+            const firstUnderlyingOut: string = "1";
+            let virtualHTokenReserves: string;
+
+            beforeEach(async function () {
+              // Do the first trade.
+              await this.contracts.hifiPool
+                .connect(this.signers.alice)
+                .buyUnderlying(this.signers.alice.address, USDC(firstUnderlyingOut));
+
+              // Calculate the amounts necessary for the second trade tests.
+              const virtualHTokenReservesBn: BigNumber = await this.contracts.hifiPool.getVirtualHTokenReserves();
+              virtualHTokenReserves = String(mbn(String(virtualHTokenReservesBn)).div("1e18"));
+            });
+
+            const testSets = [["2.141592"], ["9"], ["99"]];
+
+            forEach(testSets).it("sells %e hTokens for underlying", async function (hTokenIn: string) {
+              const underlyingReserves: string = String(mbn(totalUnderlyingReserves).sub(mbn(firstUnderlyingOut)));
+              await testBuyUnderlying.call(this, underlyingReserves, virtualHTokenReserves, hTokenIn);
+            });
+
+            forEach(testSets).it("takes %e and emits a Trade event", async function (underlyingOut: string) {
+              // Test argument equality when Waffle adds "withNamedArgs" chai matcher.
+              // https://github.com/EthWorks/Waffle/issues/437
+              await expect(
+                this.contracts.hifiPool
+                  .connect(this.signers.alice)
+                  .buyUnderlying(this.signers.alice.address, USDC(underlyingOut)),
+              ).to.emit(this.contracts.hifiPool, "Trade");
+            });
           });
         });
       });
