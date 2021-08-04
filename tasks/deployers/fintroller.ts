db48ded47455055f5f59753cc5c070a0c548870a
Paul Razvan Berg <hello@paulrberg.com>
2021-08-04 13:59:34 +0300

    chore(protocol): fix: fintroller deployer task
    chore: delete stale ALCHEMY_KEY from .env.example

--- tasks/deployers/fintroller.ts
+++ tasks/deployers/fintroller.ts
@@ -5,9 +5,7 @@ import { FintrollerV1, FintrollerV1__factory } from "../../typechain";
 
 task("deploy:Fintroller").setAction(async function (taskArguments: TaskArguments, { ethers, upgrades }) {
   const fintrollerV1Factory: FintrollerV1__factory = await ethers.getContractFactory("FintrollerV1");
-  const fintroller: FintrollerV1 = <FintrollerV1>(
-    await upgrades.deployProxy(fintrollerV1Factory, [taskArguments.fintroller, taskArguments.oracle])
-  );
+  const fintroller: FintrollerV1 = <FintrollerV1>await upgrades.deployProxy(fintrollerV1Factory);
   await fintroller.deployed();
   console.log("Fintroller deployed to: ", fintroller.address);
 });
