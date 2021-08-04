61bbc2c3aa16661ee3ef401e1df66e7d41b15379
Paul Razvan Berg <hello@paulrberg.com>
2021-08-04 17:17:30 +0300

    chore(amm): build: add polygon chain hardhat.config.ts
    refactor: createTestnetConfig to getChainConfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,11 +17,11 @@ import { getEnvVar } from "./helpers/env";
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const chainIds = {
-  ganache: 1337,
   goerli: 5,
   hardhat: 31337,
   kovan: 42,
   mainnet: 1,
+  "polygon-mainnet": 137,
   rinkeby: 4,
   ropsten: 3,
 };
@@ -30,12 +30,11 @@ const chainIds = {
 const infuraApiKey: string = getEnvVar("INFURA_API_KEY");
 const mnemonic: string = getEnvVar("MNEMONIC");
 
-function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
+function getChainConfig(network: keyof typeof chainIds): NetworkUserConfig {
   const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
   return {
     accounts: {
       count: 10,
-      initialIndex: 0,
       mnemonic,
       path: "m/44'/60'/0'/0",
     },
@@ -56,10 +55,11 @@ const config: HardhatUserConfig = {
       },
       chainId: chainIds.hardhat,
     },
-    goerli: createTestnetConfig("goerli"),
-    kovan: createTestnetConfig("kovan"),
-    rinkeby: createTestnetConfig("rinkeby"),
-    ropsten: createTestnetConfig("ropsten"),
+    goerli: getChainConfig("goerli"),
+    kovan: getChainConfig("kovan"),
+    "polygon-mainnet": getChainConfig("polygon-mainnet"),
+    rinkeby: getChainConfig("rinkeby"),
+    ropsten: getChainConfig("ropsten"),
   },
   packager: {
     contracts: ["HifiPool", "IHifiPool"],
