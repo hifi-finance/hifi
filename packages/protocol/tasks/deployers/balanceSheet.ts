b1982fc276322aae9bfb373badd3674d6dde9f41
Paul Razvan Berg <hello@paulrberg.com>
2021-08-18 15:30:50 +0300

    refactor(protocol): pretty print addresses in deploy tasks
    ci: new deployers for BalanceSheet and Fintroller
    refactor(protocol): new "printAddress" optional param for deploy tasks

--- packages/protocol/tasks/deployers/balanceSheet.ts
+++ packages/protocol/tasks/deployers/balanceSheet.ts
@@ -1,4 +1,4 @@
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
 import { BalanceSheetV1, BalanceSheetV1__factory } from "../../typechain";
@@ -6,11 +6,15 @@ import { BalanceSheetV1, BalanceSheetV1__factory } from "../../typechain";
 task("deploy:BalanceSheet")
   .addParam("fintroller", "The address of the Fintroller contract")
   .addParam("oracle", "The address of the oracle contract")
-  .setAction(async function (taskArguments: TaskArguments, { ethers, upgrades }) {
+  .addOptionalParam("printAddress", "Whether to print the address in the console", true, types.boolean)
+  .setAction(async function (taskArgs: TaskArguments, { ethers, upgrades }): Promise<string> {
     const balanceSheetV1Factory: BalanceSheetV1__factory = await ethers.getContractFactory("BalanceSheetV1");
     const balanceSheet: BalanceSheetV1 = <BalanceSheetV1>(
-      await upgrades.deployProxy(balanceSheetV1Factory, [taskArguments.fintroller, taskArguments.oracle])
+      await upgrades.deployProxy(balanceSheetV1Factory, [taskArgs.fintroller, taskArgs.oracle])
     );
     await balanceSheet.deployed();
-    console.log("BalanceSheet deployed to: ", balanceSheet.address);
+    if (taskArgs.printAddress) {
+      console.table([{ name: "BalanceSheet", address: balanceSheet.address }]);
+    }
+    return balanceSheet.address;
   });
--- packages/protocol/tasks/deployers/fintroller.ts
+++ packages/protocol/tasks/deployers/fintroller.ts
@@ -1,11 +1,16 @@
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
 import { FintrollerV1, FintrollerV1__factory } from "../../typechain";
 
-task("deploy:Fintroller").setAction(async function (taskArguments: TaskArguments, { ethers, upgrades }) {
-  const fintrollerV1Factory: FintrollerV1__factory = await ethers.getContractFactory("FintrollerV1");
-  const fintroller: FintrollerV1 = <FintrollerV1>await upgrades.deployProxy(fintrollerV1Factory);
-  await fintroller.deployed();
-  console.log("Fintroller deployed to: ", fintroller.address);
-});
+task("deploy:Fintroller")
+  .addOptionalParam("printAddress", "Whether to print the address in the console", true, types.boolean)
+  .setAction(async function (taskArgs: TaskArguments, { ethers, upgrades }) {
+    const fintrollerV1Factory: FintrollerV1__factory = await ethers.getContractFactory("FintrollerV1");
+    const fintroller: FintrollerV1 = <FintrollerV1>await upgrades.deployProxy(fintrollerV1Factory);
+    await fintroller.deployed();
+    if (taskArgs.printAddress) {
+      console.table([{ name: "Fintroller", address: fintroller.address }]);
+    }
+    return fintroller.address;
+  });
