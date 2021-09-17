27c054b592ffaf55cd9b3afd6941a90673587da1
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2021-09-17 14:06:22 +0300

    feat(deployers): track created hifiPool

--- packages/deployers/tasks/deploy/hifiPool.ts
+++ packages/deployers/tasks/deploy/hifiPool.ts
@@ -2,16 +2,19 @@ import * as core from "@actions/core";
 import { task, types } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { HifiPool__factory } from "@hifi/amm/typechain/factories/HifiPool__factory";
+import { HifiPoolRegistry__factory } from "@hifi/amm/typechain/factories/HifiPoolRegistry__factory";
 import { HifiPool } from "@hifi/amm/typechain/HifiPool";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 import { SUBTASK_DEPLOY_WAIT_FOR_CONFIRMATIONS, TASK_DEPLOY_CONTRACT_HIFI_POOL } from "../../helpers/constants";
+import { HifiPoolRegistry } from "@hifi/amm/typechain/HifiPoolRegistry";
 
 task(TASK_DEPLOY_CONTRACT_HIFI_POOL)
   // Contract arguments
   .addParam("name", "ERC-20 name of the pool token")
   .addParam("symbol", "ERC-20 symbol of the pool token")
   .addParam("hToken", "Address of the hToken contract")
+  .addParam("hifiPoolRegistry", "Address of the hifiPoolRegistry contract")
   // Developer settings
   .addOptionalParam("confirmations", "How many block confirmations to wait for", 0, types.int)
   .addOptionalParam("printAddress", "Print the address in the console", true, types.boolean)
@@ -22,6 +25,13 @@ task(TASK_DEPLOY_CONTRACT_HIFI_POOL)
     const hifiPool: HifiPool = <HifiPool>await hifiPoolFactory.deploy(taskArgs.name, taskArgs.symbol, taskArgs.hToken);
 
     await run(SUBTASK_DEPLOY_WAIT_FOR_CONFIRMATIONS, { contract: hifiPool, confirmations: taskArgs.confirmations });
+
+    const hifiPoolRegistryFactory: HifiPoolRegistry__factory = new HifiPoolRegistry__factory(signers[0]);
+    const hifiPoolRegistry: HifiPoolRegistry = <HifiPoolRegistry>(
+      hifiPoolRegistryFactory.attach(taskArgs.hifiPoolRegistry)
+    );
+
+    await hifiPoolRegistry.trackPool(hifiPool.address);
 
     if (taskArgs.setOutput) {
       core.setOutput("hifi-pool", hifiPool.address);
