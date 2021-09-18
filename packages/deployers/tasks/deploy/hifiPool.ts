b0d7658f3b53e976aa8053d8b136d01cbe876cab
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2021-09-18 11:43:16 +0300

    refactor(deployers): track pool upon deployment in HifiPool task (#48)
    * feat(deployers): track created hifiPool
    
    * feat(deployers): await trackPool tx confirmation
    
    * refactor(deployers): await block confirmations only once in HifiPool task
    
    * refactor(deployers): polish task argument descriptions in HifiPool task
    
    Co-authored-by: Paul Razvan Berg <hello@paulrberg.com>

--- packages/deployers/tasks/deploy/hifiPool.ts
+++ packages/deployers/tasks/deploy/hifiPool.ts
@@ -2,26 +2,37 @@ import * as core from "@actions/core";
 import { task, types } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { HifiPool__factory } from "@hifi/amm/typechain/factories/HifiPool__factory";
+import { HifiPoolRegistry__factory } from "@hifi/amm/typechain/factories/HifiPoolRegistry__factory";
 import { HifiPool } from "@hifi/amm/typechain/HifiPool";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-import { SUBTASK_DEPLOY_WAIT_FOR_CONFIRMATIONS, TASK_DEPLOY_CONTRACT_HIFI_POOL } from "../../helpers/constants";
+import { TASK_DEPLOY_CONTRACT_HIFI_POOL } from "../../helpers/constants";
+import { HifiPoolRegistry } from "@hifi/amm/typechain/HifiPoolRegistry";
 
 task(TASK_DEPLOY_CONTRACT_HIFI_POOL)
   // Contract arguments
   .addParam("name", "ERC-20 name of the pool token")
   .addParam("symbol", "ERC-20 symbol of the pool token")
-  .addParam("hToken", "Address of the hToken contract")
+  .addParam("hToken", "Address of the HToken contract")
+  .addParam("hifiPoolRegistry", "Address of the HifiPoolRegistry contract")
   // Developer settings
   .addOptionalParam("confirmations", "How many block confirmations to wait for", 0, types.int)
   .addOptionalParam("printAddress", "Print the address in the console", true, types.boolean)
   .addOptionalParam("setOutput", "Set the contract address as an output in GitHub Actions", false, types.boolean)
-  .setAction(async function (taskArgs: TaskArguments, { ethers, run }): Promise<string> {
+  .setAction(async function (taskArgs: TaskArguments, { ethers }): Promise<string> {
     const signers: SignerWithAddress[] = await ethers.getSigners();
     const hifiPoolFactory: HifiPool__factory = new HifiPool__factory(signers[0]);
     const hifiPool: HifiPool = <HifiPool>await hifiPoolFactory.deploy(taskArgs.name, taskArgs.symbol, taskArgs.hToken);
+    await hifiPool.deployed();
 
-    await run(SUBTASK_DEPLOY_WAIT_FOR_CONFIRMATIONS, { contract: hifiPool, confirmations: taskArgs.confirmations });
+    const hifiPoolRegistryFactory: HifiPoolRegistry__factory = new HifiPoolRegistry__factory(signers[0]);
+    const hifiPoolRegistry: HifiPoolRegistry = <HifiPoolRegistry>(
+      hifiPoolRegistryFactory.attach(taskArgs.hifiPoolRegistry)
+    );
+    const trackPoolTx = await hifiPoolRegistry.trackPool(hifiPool.address);
+    if (taskArgs.confirmations > 0) {
+      await trackPoolTx.wait(taskArgs.confirmations);
+    }
 
     if (taskArgs.setOutput) {
       core.setOutput("hifi-pool", hifiPool.address);
