ffda1795cf11185e0a76d325ad6cdc6f48af6fb6
Paul Razvan Berg <hello@paulrberg.com>
2021-11-04 11:17:15 +0200

    refactor: move common logic in "HifiFlashUniswapV2.sol"

--- packages/flash-swap/contracts/uniswap-v2/HifiFlashUniswapV2Underlying.sol
+++ packages/flash-swap/contracts/uniswap-v2/HifiFlashUniswapV2Underlying.sol
@@ -7,6 +7,7 @@ import "@hifi/protocol/contracts/core/balanceSheet/IBalanceSheetV1.sol";
 import "@hifi/protocol/contracts/core/balanceSheet/SBalanceSheetV1.sol";
 import "@hifi/protocol/contracts/core/hToken/IHToken.sol";
 
+import "./HifiFlashUniswapV2Utils.sol";
 import "./IHifiFlashUniswapV2Underlying.sol";
 import "./IUniswapV2Pair.sol";
 
@@ -137,13 +138,22 @@ contract HifiFlashUniswapV2Underlying is IHifiFlashUniswapV2Underlying {
             : IUniswapV2Pair(msg.sender).token0();
 
         // Check that the caller is a genuine UniswapV2Pair contract.
-        if (msg.sender != pairFor(address(vars.underlying), vars.swapToken)) {
+        if (
+            msg.sender !=
+            HifiFlashUniswapV2Utils.pairFor(
+                uniV2Factory,
+                uniV2PairInitCodeHash,
+                address(vars.underlying),
+                vars.swapToken
+            )
+        ) {
             revert HifiFlashUniswapV2Underlying__CallNotAuthorized(msg.sender);
         }
 
         // Mint hTokens and liquidate the borrower.
-        vars.mintedHTokenAmount = mintHTokensInternal(vars.bond, vars.underlyingAmount);
-        vars.seizedCollateralAmount = liquidateBorrowInternal(
+        vars.mintedHTokenAmount = HifiFlashUniswapV2Utils.mintHTokensInternal(vars.bond, vars.underlyingAmount);
+        vars.seizedCollateralAmount = HifiFlashUniswapV2Utils.liquidateBorrowInternal(
+            balanceSheet,
             vars.borrower,
             vars.bond,
             vars.collateral,
@@ -173,77 +183,5 @@ contract HifiFlashUniswapV2Underlying is IHifiFlashUniswapV2Underlying {
             vars.seizedCollateralAmount,
             vars.repayCollateralAmount
         );
-    }
-
-    /// INTERNAL CONSTANT FUNCTIONS ///
-
-    /// @dev Calculates the CREATE2 address for a pair without making any external calls.
-    function pairFor(address tokenA, address tokenB) internal view returns (address pair) {
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        pair = address(
-            uint160(
-                uint256(
-                    keccak256(
-                        abi.encodePacked(
-                            hex"ff",
-                            uniV2Factory,
-                            keccak256(abi.encodePacked(token0, token1)),
-                            uniV2PairInitCodeHash
-                        )
-                    )
-                )
-            )
-        );
-    }
-
-    /// INTERNAL NON-CONSTANT FUNCTIONS ///
-
-    /// @dev Liquidates the borrower by transferring the underlying to the BalanceSheet. By doing this, the
-    /// liquidator receives collateral at a discount.
-    function liquidateBorrowInternal(
-        address borrower,
-        IHToken bond,
-        IErc20 collateral,
-        uint256 mintedHTokenAmount
-    ) internal returns (uint256 seizedCollateralAmount) {
-        uint256 collateralAmount = balanceSheet.getCollateralAmount(borrower, collateral);
-        uint256 hypotheticalRepayAmount = balanceSheet.getRepayAmount(collateral, collateralAmount, bond);
-
-        // If the hypothetical repay amount is bigger than the debt amount, this could be a single-collateral multi-bond
-        // vault. Otherwise, it could be a multi-collateral single-bond vault. However, it is difficult to generalize
-        // for the multi-collateral and multi-bond situation. The repay amount could be either bigger, smaller, or even
-        // equal to the debt amount depending on the collateral and debt amount distribution.
-        uint256 debtAmount = balanceSheet.getDebtAmount(borrower, bond);
-        uint256 repayAmount = hypotheticalRepayAmount > debtAmount ? debtAmount : hypotheticalRepayAmount;
-
-        // Truncate the repay amount such that we keep the dust in this contract rather than the BalanceSheet.
-        uint256 truncatedRepayAmount = mintedHTokenAmount > repayAmount ? repayAmount : mintedHTokenAmount;
-
-        // Liquidate borrow.
-        uint256 oldCollateralBalance = collateral.balanceOf(address(this));
-        balanceSheet.liquidateBorrow(borrower, bond, truncatedRepayAmount, collateral);
-        uint256 newCollateralBalance = collateral.balanceOf(address(this));
-        unchecked {
-            seizedCollateralAmount = newCollateralBalance - oldCollateralBalance;
-        }
-    }
-
-    /// @dev Supplies the underlying to the HToken contract to mint hTokens without taking on debt.
-    function mintHTokensInternal(IHToken bond, uint256 underlyingAmount) internal returns (uint256 mintedHTokenAmount) {
-        IErc20 underlying = bond.underlying();
-
-        // Allow the HToken contract to spend underlying if allowance not enough.
-        uint256 allowance = underlying.allowance(address(this), address(bond));
-        if (allowance < underlyingAmount) {
-            underlying.approve(address(bond), type(uint256).max);
-        }
-
-        // Mint hTokens.
-        uint256 preHTokenBalance = bond.balanceOf(address(this));
-        bond.supplyUnderlying(underlyingAmount);
-        uint256 postHTokenBalance = bond.balanceOf(address(this));
-        unchecked {
-            mintedHTokenAmount = postHTokenBalance - preHTokenBalance;
-        }
     }
 }
---
+++ packages/flash-swap/contracts/uniswap-v2/HifiFlashUniswapV2Utils.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+import "@paulrberg/contracts/token/erc20/IErc20.sol";
+import "@hifi/protocol/contracts/core/balanceSheet/IBalanceSheetV1.sol";
+
+/// @title HifiFlashUniswapV2
+/// @author Hifi
+library HifiFlashUniswapV2Utils {
+    /// @dev The init code hash of the UniswapV2Pair contract. The same for all Uniswap v2 deployments.
+    bytes32 public constant UNI_V2_PAIR_INIT_CODE_HASH =
+        0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f;
+
+    /// @dev Calculates the CREATE2 address for a pair without making any external calls.
+    function pairFor(
+        address uniV2Factory,
+        bytes32 uniV2PairInitCodeHash,
+        address tokenA,
+        address tokenB
+    ) internal pure returns (address pair) {
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        pair = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            hex"ff",
+                            uniV2Factory,
+                            keccak256(abi.encodePacked(token0, token1)),
+                            uniV2PairInitCodeHash
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    /// @dev Liquidates the borrower by transferring the underlying to the BalanceSheet. By doing this, the
+    /// liquidator receives collateral at a discount.
+    function liquidateBorrowInternal(
+        IBalanceSheetV1 balanceSheet,
+        address borrower,
+        IHToken bond,
+        IErc20 collateral,
+        uint256 mintedHTokenAmount
+    ) internal returns (uint256 seizedCollateralAmount) {
+        uint256 collateralAmount = balanceSheet.getCollateralAmount(borrower, collateral);
+        uint256 hypotheticalRepayAmount = balanceSheet.getRepayAmount(collateral, collateralAmount, bond);
+
+        // If the hypothetical repay amount is bigger than the debt amount, this could be a single-collateral multi-bond
+        // vault. Otherwise, it could be a multi-collateral single-bond vault. However, it is difficult to generalize
+        // for the multi-collateral and multi-bond situation. The repay amount could be either bigger, smaller, or even
+        // equal to the debt amount depending on the collateral and debt amount distribution.
+        uint256 debtAmount = balanceSheet.getDebtAmount(borrower, bond);
+        uint256 repayAmount = hypotheticalRepayAmount > debtAmount ? debtAmount : hypotheticalRepayAmount;
+
+        // Truncate the repay amount such that we keep the dust in this contract rather than the BalanceSheet.
+        uint256 truncatedRepayAmount = mintedHTokenAmount > repayAmount ? repayAmount : mintedHTokenAmount;
+
+        // Liquidate borrow.
+        uint256 oldCollateralBalance = collateral.balanceOf(address(this));
+        balanceSheet.liquidateBorrow(borrower, bond, truncatedRepayAmount, collateral);
+        uint256 newCollateralBalance = collateral.balanceOf(address(this));
+        unchecked {
+            seizedCollateralAmount = newCollateralBalance - oldCollateralBalance;
+        }
+    }
+
+    /// @dev Supplies the underlying to the HToken contract to mint hTokens without taking on debt.
+    function mintHTokensInternal(IHToken bond, uint256 underlyingAmount) internal returns (uint256 mintedHTokenAmount) {
+        IErc20 underlying = bond.underlying();
+
+        // Allow the HToken contract to spend underlying if allowance not enough.
+        uint256 allowance = underlying.allowance(address(this), address(bond));
+        if (allowance < underlyingAmount) {
+            underlying.approve(address(bond), type(uint256).max);
+        }
+
+        // Mint hTokens.
+        uint256 preHTokenBalance = bond.balanceOf(address(this));
+        bond.supplyUnderlying(underlyingAmount);
+        uint256 postHTokenBalance = bond.balanceOf(address(this));
+        unchecked {
+            mintedHTokenAmount = postHTokenBalance - preHTokenBalance;
+        }
+    }
+}
