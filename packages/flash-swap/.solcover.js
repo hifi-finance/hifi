d23cc03e73f5107f14fa70bcd66c2280ef47b2d2
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@proton.me>
2023-12-11 10:43:17 -0700

    chore(flash-swap): skip test coverage for "uniswap-v3/libraries" dir chore(flash-swap): re-generate types

--- packages/flash-swap/.solcover.js
+++ packages/flash-swap/.solcover.js
@@ -7,7 +7,7 @@ module.exports = {
     "uniswap-v2/IUniswapV2Pair.sol",
     "uniswap-v2/UniswapV2Pair.sol",
     "uniswap-v2/test",
-    "uniswap-v3/NoDelegateCall.sol",
+    "uniswap-v3/libraries",
     "uniswap-v3/UniswapV3Pool",
     "uniswap-v3/test",
   ],
